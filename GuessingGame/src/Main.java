8eda727c491bc326e15275def8c5de621e79923e
BasarEkinci <basarekinci02@outlook.com>
2023-03-07 17:10:40 +0300

    Variables
    Variables added

--- GuessingGame/src/Main.java
+++ GuessingGame/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int randomNumber;
+        int guessNumber;
+        int operation;
+        int chances;
+        int difficulty;
+
+        
+
     }
 }
