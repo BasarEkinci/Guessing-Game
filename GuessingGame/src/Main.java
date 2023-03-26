7f5014270ba35965428280f8c52f3238461f3a48
BasarEkinci <basarekinci02@outlook.com>
2023-03-26 14:58:58 +0300

    Edit

--- GuessingGame/src/Main.java
+++ GuessingGame/src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 import java.util.Random;
 public class Main {
@@ -7,9 +8,9 @@ public class Main {
     static Scanner input = new Scanner(System.in);
     static int randomNumber;
     static int guessNumber;
-    static int chances;
-    static int numberScore;
-    static int letterScore;
+    static int wrongRight;
+    static int guessTheNumberScore;
+    static int guessTheAlphabetScore;
     static int menuInput;
     static int startMenuInput;
     static int counter;
@@ -29,12 +30,99 @@ public class Main {
                     do{
                         System.out.println("---------------\nGuess The Number\n---------------");
                         StartMenu();
-                        if(startMenuInput == 1){
-                            EasyNumberGuess();
-                        }else if(startMenuInput == 2){
-                            MediumNumberGuess();
-                        }else if(startMenuInput == 3) {
-                            HardNumberGuess();
+                        if(startMenuInput == 1){//Guess the Number Easy
+                            randomNumber = random.nextInt(15);
+                            counter = 0;
+                            wrongRight = 5;
+                            GuessTheNumberInstructions(1);
+                            System.out.println("\nGood Luck!");
+                            System.out.println("Easy:");
+
+                            do{
+                                System.out.println("Enter guess number from 1 to 15. " + wrongRight + " chances left");
+                                System.out.print("Guess number: ");
+                                guessNumber = input.nextInt();
+
+                                if(guessNumber == randomNumber){
+                                    counter++;
+                                    guessTheNumberScore += 5;
+                                    System.out.println("Correct!You've won the game just " + counter + " times.\n");
+                                }else if(guessNumber < randomNumber){
+                                    wrongRight--;
+                                    counter++;
+                                    System.out.println("Wrong!Your guess was too low\n");
+                                }else if (guessNumber > randomNumber){
+                                    wrongRight--;
+                                    counter++;
+                                    System.out.println("Wrong!Your guess was too high\n");
+                                }
+
+                                if(wrongRight == 0){
+                                    System.out.println("\nYou lost the game! ");
+                                }
+                            }while (wrongRight != 0 && guessNumber != randomNumber);
+
+                        }else if(startMenuInput == 2){//Guess the number : medium
+                            randomNumber = random.nextInt(20);
+                            wrongRight = 4;
+                            counter = 0;
+                            GuessTheNumberInstructions(2);
+                            System.out.println("\nGood Luck!");
+                            System.out.println("Medium:");
+
+                            do{
+                                System.out.println("Enter guess number from 1 to 20. " + wrongRight + " chances left");
+                                System.out.print("Guess number: ");
+                                guessNumber = input.nextInt();
+                                if(guessNumber == randomNumber){
+                                    counter++;
+                                    guessTheNumberScore += 10;
+                                    System.out.println("Correct!You've won the game just " + counter + " times.\n");
+                                }else if(guessNumber < randomNumber){
+                                    wrongRight--;
+                                    counter++;
+                                    System.out.println("Wrong!Your guess was too low\n");
+                                }else if (guessNumber > randomNumber){
+                                    wrongRight--;
+                                    counter++;
+                                    System.out.println("Wrong!Your guess was too high\n");
+                                }
+
+                                if(wrongRight == 0){
+                                    System.out.println("\nYou lost the game! ");
+                                }
+                            }while (wrongRight != 0 && guessNumber != randomNumber);
+
+                        }else if(startMenuInput == 3) {//Guess the Number : Hard
+                            randomNumber = random.nextInt(40);
+                            wrongRight = 4;
+                            counter = 0;
+                            GuessTheNumberInstructions(3);
+                            System.out.println("\nGood Luck!");
+                            System.out.println("Medium:");
+
+                            do{
+                                System.out.println("Enter guess number from 1 to 40. " + wrongRight + " chances left");
+                                System.out.print("Guess number: ");
+                                guessNumber = input.nextInt();
+                                if(guessNumber == randomNumber){
+                                    counter++;
+                                    guessTheNumberScore += 15;
+                                    System.out.println("Correct!You've won the game just " + counter + " times.\n");
+                                }else if(guessNumber < randomNumber){
+                                    wrongRight--;
+                                    counter++;
+                                    System.out.println("Wrong!Your guess was too low\n");
+                                }else if (guessNumber > randomNumber){
+                                    wrongRight--;
+                                    counter++;
+                                    System.out.println("Wrong!Your guess was too high\n");
+                                }
+
+                                if(wrongRight == 0){
+                                    System.out.println("\nYou lost the game! ");
+                                }
+                            }while (wrongRight != 0 && guessNumber != randomNumber);
                         }else if(startMenuInput == 4){
                             System.out.println("--Returning Main Menu--\n-----------------------");
                         }
@@ -62,16 +150,16 @@ public class Main {
                     }while (startMenuInput != 4);
                     break;
                 case 3://Show Score
-                    if(letterScore == 0)
+                    if(guessTheAlphabetScore == 0)
                         System.out.println("Guess the Alphabet Score: Have not played yet");
                     else
-                        System.out.println("Guess The Alphabet Score: " + letterScore);
-                    if(numberScore == 0)
+                        System.out.println("Guess The Alphabet Score: " + guessTheAlphabetScore);
+                    if(guessNumber == 0)
                         System.out.println("Guess the Number Score: Have not played yet");
                     else
-                        System.out.println("Guess The Number Score: " + numberScore);
+                        System.out.println("Guess The Number Score: " + guessTheNumberScore);
 
-                    System.out.println("Total score: " + (numberScore + letterScore));//Total Score
+                    System.out.println("Total score: " + (guessTheAlphabetScore + guessTheNumberScore));//Total Score
                     break;
                 case 4://Show Credit
                     ShowCredit();
@@ -103,50 +191,41 @@ public class Main {
     }
     static void GuessTheNumberInstructions(int difficulty){
 
-        if(difficulty == 1)
-            chances = 5;
+        if(difficulty == 1)//According to the difficulty to choose the wrong right
+            wrongRight = 5;
         else if(difficulty == 2)
-            chances = 4;
+            wrongRight = 4;
         else if(difficulty == 3)
-            chances = 3;
+            wrongRight = 3;
 
         System.out.println("Instructions: ");
         System.out.println("1. Guess the number based on the gaven range.");
         System.out.println("2. You are allowed to make one guess at a time.");
-        System.out.println("3. Each game has " + chances + " chances to guess the correct number.");
+        System.out.println("3. Each game has " + wrongRight + " chances to guess the correct number.");
         System.out.println("4. Once you have used up all your chances, you lose the game.");
     }
     static void GuessTheAlphabetInstructions(int difficulty){
-        if(difficulty == 1){
-            chances = 2;
+        if(difficulty == 1){//According to the difficulty to choose instructions
+            wrongRight = 2;
             System.out.println("1. Guess The vowel alphabet");
             System.out.println("2. You are allowed to make one guess a time");
-            System.out.println("3. Each game has " + chances + " chances to guess the correct vowel");
+            System.out.println("3. Each game has " + wrongRight + " chances to guess the correct vowel");
             System.out.println("4. Once you have used up all your chances, you lose the game");
         }
         if(difficulty == 2){
-            chances = 4;
+            wrongRight = 4;
             System.out.println("1. Guess The consonant alphabet");
             System.out.println("2. You are allowed to make one guess a time");
-            System.out.println("3. Each game has " + chances + " chances to guess the correct vowel");
+            System.out.println("3. Each game has " + wrongRight + " chances to guess the correct vowel");
             System.out.println("4. Once you have used up all your chances, you lose the game");
         }
         if(difficulty == 3){
-            chances = 5;
+            wrongRight = 5;
             System.out.println("1. Guess The alphabet");
             System.out.println("2. You are allowed to make one guess a time");
-            System.out.println("3. Each game has " + chances + " chances to guess the correct vowel");
+            System.out.println("3. Each game has " + wrongRight + " chances to guess the correct vowel");
             System.out.println("4. Once you have used up all your chances, you lose the game");
         }
-    }
-    static void EasyNumberGuess(){
-        NumberGuessGameEvents(1);
-    }
-    static void MediumNumberGuess(){
-        NumberGuessGameEvents(2);
-    }
-    static void HardNumberGuess(){
-        NumberGuessGameEvents(3);
     }
     static void EasyAlphabetGuess(){
         AlphabetGuessGameEvents(1);
@@ -157,7 +236,7 @@ public class Main {
     static void HardAlphabetGuess(){
         AlphabetGuessGameEvents(3);
     }
-    static void AlphabetGuessGameEvents(int difficulty){
+    static void AlphabetGuessGameEvents(int difficulty){//to set score multiplier, difficulty and instructions
 
         GuessTheAlphabetInstructions(difficulty);
         String letters = "";
@@ -176,67 +255,34 @@ public class Main {
 
         guessAlphabet = letters.charAt(random.nextInt(letters.length()));
 
-        while (chances > 0 && isGameContinue){
-            System.out.println("Enter a word: " + chances + " left.What is the alphabet.");
+        while (wrongRight > 0 && isGameContinue){
+            System.out.println("Enter a word: " + wrongRight + " left.What is the alphabet.");
             System.out.print("===> ");
             guessAlphabet = input.next().charAt(0);
             if(guessAlphabet == randomAlphabet){
                 System.out.println("Correct! You won the game!");
-                letterScore += scoreMultiplier;
+                guessTheAlphabetScore += scoreMultiplier;
                 isGameContinue = false;
             }else{
                 System.out.println("Wrong guess!");
-                chances--;
+                wrongRight--;
             }
         }
-        if(chances == 0){
+        if(wrongRight == 0){
             System.out.println("You lost the game");
         }
     }
-    static void NumberGuessGameEvents(int difficulty){
-        GuessTheNumberInstructions(difficulty);
-        System.out.println("Good Luck! ");
-        int bound = 0;
-        int scoreMultiplier = 0;
-        if(difficulty == 1){
-            scoreMultiplier = 10;
-            bound = 15;
-        }else if(difficulty == 2){
-            scoreMultiplier = 15;
-            bound = 20;
-        }else if(difficulty == 3){
-            scoreMultiplier = 20;
-            bound = 40;
-        }
-        randomNumber = random.nextInt(bound);
 
-        while (chances > 0 && isGameContinue){
-            System.out.println("Enter your guess number from 1 to " + bound + ". " + chances + " chances left.");
-            System.out.print("==> ");
-            guessNumber = input.nextInt();
-
-            if(guessNumber > randomNumber){
-                System.out.println("Wrong! Your guess is higher.");
-                counter++;
-                chances--;
-            }else if(guessNumber < randomNumber){
-                System.out.println("Wrong! Your guess is lower");
-                counter++;
-                chances--;
-            }else if(randomNumber == guessNumber){
-                System.out.println("Correct! You've won the game with just " + counter + " times");
-                counter++;
-                numberScore += scoreMultiplier;
-                isGameContinue = false;
-            }
-        }
-        if(chances == 0){
-            System.out.println("You lost the game!");
-            counter = 0;
-        }
-    }
     static void ShowCredit(){
-        Student s1 = new Student("Ismail Basar","Ekinci","200209054","Software Construction");
-        System.out.println(s1.ToString());
+        System.out.println("Student Name/Surname && ID");
+        System.out.println("Gulsu Oz - 200209057");
+        System.out.println("Huseyin Murat - ");
+        System.out.println("Omer Akce - ");
+        System.out.println("Ismail Basar Ekinci - 200209057");
+        System.out.println("Edar - ");
+        System.out.println("Emre - ");
+        System.out.println("Course of Study: Software Construction");
     }
+
+
 }
